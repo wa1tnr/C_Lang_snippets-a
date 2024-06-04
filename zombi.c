580364ebd41dfc4f7141f0cba9d34dcf3feba0a1
wa1tnr <wa1tnr@users.noreply.github.com>
2024-06-04 19:28:31 +0000

    printf breakdowns

--- zombi.c
+++ zombi.c
@@ -7,10 +7,23 @@ int main() {
     printf("%s", "\tuint8_t test[4] = { 22, -44, -88, 44 };\r\n");
 
     printf("%s", "\ttest[0]: ");
-    printf("%b", test[0]);
+    printf("%b\r\n\r\n", test[0]);
 
+    printf("\t%s", "test[1] holds -44:\r\n");
     printf("%s", "\ttest[1]: ");
-    printf("%b", test[1]);
+    printf("%b\r\n", test[1]);
+
+    int16_t p = (int16_t) test[1];
+    int16_t q = 256 - p;
+    int8_t  r = 212;
+    int8_t  s = (int8_t) 212;
+    int16_t t = 212;
+
+    printf("\t      p: %b\r\n", p);
+    printf("\t      q: %b\r\n", q);
+    printf("\t      r: %b\r\n", r);
+    printf("\t      s: %b\r\n", s);
+    printf("\t      t: %b\r\n", t);
 
     printf("%s\n", "");
     for (int i = 0; i < 4; i++) {

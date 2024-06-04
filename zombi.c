b78491ef308e15a151bc39dd35233d706af58275
wa1tnr <wa1tnr@users.noreply.github.com>
2024-06-04 18:13:48 +0000

    added binary out for test_1

--- zombi.c
+++ zombi.c
@@ -4,6 +4,15 @@
 int main() {
     uint8_t test[4] = { 22, -44, -88, 44 };
 
+    printf("%s", "\tuint8_t test[4] = { 22, -44, -88, 44 };\r\n");
+
+    printf("%s", "\ttest[0]: ");
+    printf("%b", test[0]);
+
+    printf("%s", "\ttest[1]: ");
+    printf("%b", test[1]);
+
+    printf("%s\n", "");
     for (int i = 0; i < 4; i++) {
         int16_t temp = (test[i] << 8 >> 8);
         printf("%d\n", temp);

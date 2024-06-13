a0036b0d8d00951282702399241b4d8df05026de
wa1tnr <wa1tnr@users.noreply.github.com>
2024-06-13 18:03:12 +0000

    forgotten newfile

---
+++ int8_t-exp-aa.d/esp32.d/esp32-ff/esp32-ff/cpp_macros.h
@@ -0,0 +1,22 @@
+/* cpp_macros.h*/
+
+#define print_faked() \
+  print_me(); \
+  buf_ptr =(char *) &buffer; \
+  pushed = (int) buf_ptr; \
+  push(pushed - POFFSET); \
+  rdumps(); \
+  tossed = pop(); \
+  if (tossed == -715) Serial.println("NEVER");
+
+#define print_faked_cr() \
+  print_me(); \
+  print_cr(); \
+  buf_ptr =(char *) &buffer; \
+  pushed = (int) buf_ptr; \
+  push(pushed - POFFSET); \
+  rdumps(); \
+  tossed = pop(); \
+  if (tossed == -715) Serial.println("NEVER_CR");
+
+/* end. */

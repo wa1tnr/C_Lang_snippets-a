52a6da246049a0d20199a6ee2b9afd96b3b1c065
wa1tnr <wa1tnr@users.noreply.github.com>
2024-06-15 21:18:19 +0000

    clang fmt

--- int8_t-exp-aa.d/esp32.d/esp32-ff/esp32-ff/cpp_macros.h
+++ int8_t-exp-aa.d/esp32.d/esp32-ff/esp32-ff/cpp_macros.h
@@ -1,22 +1,24 @@
 /* cpp_macros.h*/
 
-#define print_faked() \
-  print_me(); \
-  buf_ptr =(char *) &buffer; \
-  pushed = (int) buf_ptr; \
-  push(pushed - POFFSET); \
-  rdumps(); \
-  tossed = pop(); \
-  if (tossed == -715) Serial.println("NEVER");
+#define print_faked()                                                          \
+    print_me();                                                                \
+    buf_ptr = (char *)&buffer;                                                 \
+    pushed = (int)buf_ptr;                                                     \
+    push(pushed - POFFSET);                                                    \
+    rdumps();                                                                  \
+    tossed = pop();                                                            \
+    if (tossed == -715)                                                        \
+        Serial.println("NEVER");
 
-#define print_faked_cr() \
-  print_me(); \
-  print_cr(); \
-  buf_ptr =(char *) &buffer; \
-  pushed = (int) buf_ptr; \
-  push(pushed - POFFSET); \
-  rdumps(); \
-  tossed = pop(); \
-  if (tossed == -715) Serial.println("NEVER_CR");
+#define print_faked_cr()                                                       \
+    print_me();                                                                \
+    print_cr();                                                                \
+    buf_ptr = (char *)&buffer;                                                 \
+    pushed = (int)buf_ptr;                                                     \
+    push(pushed - POFFSET);                                                    \
+    rdumps();                                                                  \
+    tossed = pop();                                                            \
+    if (tossed == -715)                                                        \
+        Serial.println("NEVER_CR");
 
 /* end. */

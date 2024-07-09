3a96e30140d3e795288c087b9e11a2490a73db51
wa1tnr <wa1tnr@users.noreply.github.com>
2024-07-09 19:08:07 +0000

    endless loop but full program

---
+++ int8_t-exp-aa.d/stm32.d/24hToAMPM-aa/stm32_24_to_12h_itoa_method.cpp
@@ -0,0 +1,37 @@
+
+/* program */
+
+void report(int prcount, int prp) {
+  char buffer[32];
+  snprintf(buffer, sizeof buffer, "  %02d %02d  ", prcount, prp);
+  Serial.print(buffer);
+  delay(800);
+}
+
+void timeMaths() {
+  int count = -1;
+  for (;;) {
+    count++;
+    int p = count % 12;
+    int prp = p + (p == 0) * 12;
+    count = (count % 24);
+    report(count, prp);
+  }
+}
+
+void setupSerial() {
+  Serial.begin(115200);
+  delay(500);
+  while (!Serial)
+    ;
+  Serial.write('\n');
+}
+
+void setup() {
+  delay(1900);
+  setupSerial();
+  timeMaths();
+}
+
+void loop() { ; }
+/* end of program. */

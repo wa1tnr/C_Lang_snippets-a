9e69cf93a8d1609c9782f0499b50229517d56fad
wa1tnr <wa1tnr@users.noreply.github.com>
2024-09-09 19:53:18 +0000

    may be enough to build and test
      unknown state
      rapid port in
    
    	new file:   hoosg.h
    	new file:   main.cpp
    
    On branch dvlp-aa

---
+++ pbswitch-a.d/examples/uno/src/hoosg.h
@@ -0,0 +1,59 @@
+/* hoosg.h library or include */
+
+#include "timeWasters.h"
+
+const int pinLED = 8;
+const int pbSwitch = 9;
+#define SHOWN_BLINKS 7
+const int testBlinkCount = (SHOWN_BLINKS * 2) + 2;
+
+bool state = true;
+
+void toggleLED() {
+  state = !state;
+  digitalWrite(pinLED, state);
+}
+
+void sendToLEDDirect() { toggleLED(); }
+
+void debouncePbSw() {
+  delay(10); // TODO - cheating!
+}
+
+bool scanPbSwitch() {
+  int pbSwState = digitalRead(pbSwitch);
+  return pbSwState;
+}
+
+void blink() {
+  for (int iterations = testBlinkCount; iterations > 1; iterations--) {
+    toggleLED();
+    waitSome();
+  }
+}
+
+void initGPIO() {
+  pinMode(pinLED, OUTPUT);
+  pinMode(pbSwitch, INPUT);
+}
+
+void initSerial() {
+  Serial.begin(9600);
+  Serial.println("hi");
+  Serial.print(__DATE__);
+  Serial.write(' ');
+  Serial.print(__TIME__);
+  Serial.write('z');
+  Serial.print("\n\n");
+}
+
+/* result: LED properly wired */
+void testLED() {
+  digitalWrite(pinLED, 1);
+  while (-1)
+    ;
+}
+
+/* Mon  9 Sep 19:52:08 UTC 2024 */
+/* kanamaris */
+/* end */
---
+++ pbswitch-a.d/examples/uno/src/main.cpp
@@ -0,0 +1,82 @@
+/* main.cpp - main program 9 September 2024 */
+
+#include "hoosg.h"
+#include <Arduino.h>
+
+extern boolean state;
+extern void toggleLED();
+extern void waitLess();
+extern void waitSome();
+extern void blink();
+extern void initSerial();
+extern void initGPIO();
+
+/* result: LED properly wired */
+extern void testLED();
+// extern int readPbSwitch();
+extern bool scanPbSwitch();
+
+void setup() {
+  initSerial();
+  initGPIO();
+  blink();
+  Serial.println(" blink() already ran.");
+}
+
+void newLine() { Serial.write('\n'); }
+
+int counter = -1;
+
+void actuate() {
+  counter++;
+  newLine();
+  Serial.print(counter);
+  Serial.print("     TADA");
+}
+
+bool readingSwitch() {
+  bool held = false;
+  held = scanPbSwitch();
+  return held;
+}
+
+int hbeat = -1;
+int iHBeat = -1;
+
+void showHBeat() {
+  Serial.write(' ');
+  Serial.write('.');
+  Serial.write(' ');
+}
+
+void iHeartbeat() {
+  int hbInterval = (128 - 10); /* 16384 */
+  hbeat++;
+  if (hbeat > hbInterval) {
+    showHBeat();
+    hbeat = -1;
+  }
+}
+
+void heartbeat() {
+  int iHbIval = (4096 - 10);
+  iHBeat++;
+  if (iHBeat > iHbIval) {
+    iHeartbeat();
+    iHBeat = -1;
+  }
+}
+
+void loop() {
+  bool switchActive = false;
+  switchActive = readingSwitch();
+  if (switchActive == true) {
+    actuate();
+    switchActive = false;
+  }
+  heartbeat();
+}
+
+/* reformat ctrl shift i as in india which F1 menu tells you */
+
+/* end */

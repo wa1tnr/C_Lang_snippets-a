e90fd5df8c964bf97ab411424ce9e9ddf374b7f5
wa1tnr <wa1tnr@users.noreply.github.com>
2024-09-09 19:48:58 +0000

    blind commit recent project

---
+++ pbswitch-a.d/examples/uno/timeWasters.h
@@ -0,0 +1,19 @@
+/* timeWasters.h */
+
+void nopp() {
+  // empty function
+}
+
+void waitLess() {
+  for (volatile int k = 299; k > 1; k--) {
+    nopp();
+  }
+}
+
+void waitSome() {
+  for (volatile int j = 999; j > 1; j--) {
+    waitLess();
+  }
+}
+
+/* end */

015cd066dfac2a449ff0327cf2d725d5ddc33674
wa1tnr <wa1tnr@users.noreply.github.com>
2024-09-10 01:41:33 +0000

    new consts dotH file

---
+++ pbswitch-a.d/examples/uno/src/consts.h
@@ -0,0 +1,9 @@
+/* consts.h  10 September 2024 */
+#ifndef CONSTS_HFILE
+#define CONSTS_HFILE
+const int pbSwitch = 9;
+const int pinLED = 8;
+#define SHOWN_BLINKS 7
+const int testBlinkCount = (SHOWN_BLINKS * 2) + 2;
+#endif
+/* end */
